8101df1c7a62481f65463fe9488721ef0348c5b9
Deepak <deepak.negi343@gmail.com>
2020-08-25 20:28:01 +0530

    Updates

---
+++ precache-manifest.388d4247f2afd746b09bc6da47a223cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f75649269b57257fb5c72740592500b",
+    "url": "/covid_react/index.html"
+  },
+  {
+    "revision": "91fda215fd042c1bde6c",
+    "url": "/covid_react/static/css/main.0c092302.chunk.css"
+  },
+  {
+    "revision": "1d5f92a0e01e7ed9ace1",
+    "url": "/covid_react/static/js/2.275cbd6e.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid_react/static/js/2.275cbd6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91fda215fd042c1bde6c",
+    "url": "/covid_react/static/js/main.81dcd9fb.chunk.js"
+  },
+  {
+    "revision": "97135b056c84c517ab6f",
+    "url": "/covid_react/static/js/runtime-main.de64a0d4.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid_react/static/media/image.d7265326.png"
+  }
+]);
